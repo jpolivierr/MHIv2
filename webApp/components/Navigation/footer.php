0727ae320452cad708dc89a721b024cc229293d9
frederic <jpolivierr@gmail.com>
2022-09-15 11:53:12 -0400

    created footer component

---
+++ webApp/components/Navigation/footer.php
@@ -0,0 +1,22 @@
+<?php (function(){ ob_start(); ?>
+ <article class="av-article-v1 sp column-3x av-padding-2x av-padding-flush-top" >
+        <div class="container">
+            <section >
+                   <i class="fas fa-school"></i>
+                    <h4>Prepare The Youth</h4>
+                    <p>Our principal focus is supporting the Ecole Academie Solitarite du Nord school in Cap-Haitien. This school currently educates 150 children.</p>
+            </section>
+            <section >
+                    <i class="fas fa-user-graduate"></i>
+                    <h4>Provide Education</h4>
+                    <p>Our goal is to increase the numbers of children and staff and ultimately expand the curriculum to more vocational and academic pathways.</p>
+            </section>
+            <section >
+                   <i class="fas fa-chalkboard-teacher"></i>
+                    <h4>Provide Resources</h4>
+                    <p>In addition of school supplies, we also want to be able to provide two meals a day for our school children, as many of them only eat once a day.</p>
+            </section>
+        </div>
+    </article>
+    
+ <?php echo ob_get_clean(); })(); ?>
--- webApp/index.php
+++ webApp/index.php
@@ -6,11 +6,13 @@
 
     addHeader("header", "MHI");
 
-     mount("Navigation","navigation", "navContent");
+     mount("Navigation","header", "navContent");
      mount("Article","empower", null);
+     mount("Article","goal", null);
      mount("Article","mission", null);
      mount("Article","pledge", null);
      mount("Article","social", null);
+     
 
 
 
