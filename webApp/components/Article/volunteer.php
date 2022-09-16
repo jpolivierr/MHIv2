6fa0cc52681ef18e8a4d5c7f9272c398c998d287
frederic <jpolivierr@gmail.com>
2022-09-16 13:43:46 -0400

    completed the Volunteer page

--- webApp/components/Article/volunteer.php
+++ webApp/components/Article/volunteer.php
@@ -1,18 +1,56 @@
 <?php (function(){ ob_start(); ?>
- <article class="av-article-v1 sp center-content bk-light av-padding-1x ">
+ <article style="background:url('/Media/images/kids-background.png') center center/cover ;" class="av-article-v1 column-1x sp av-padding-2x volunteer">
         <div class="container">
-            <section >
-            <h4>News Letter</h4>
+            <section class="main-shadow">
+            <h2>Volunteer</h2>
                     <form action="">
+                        <legend>MHI relies on volunteers in every aspect of our organization and values
+the time, talents, and passions volunteers contribute to achieving of our mission, through leadership,
+programs, special projects and serving the public.</legend>
+                       <h4>Volunteer Information</h4>
                         <fieldset>
-                            <label>Full Name</label>
-                            <input type="text" placeholder="full Name"/>
+                            <label>First Name</label>
+                            <input type="text"/>
+                        </fieldset>
+                        <fieldset>
+                            <label>Last Name</label>
+                            <input type="text"/>
                         </fieldset>
                         <fieldset>
                             <label>Email</label>
-                            <input type="email" placeholder="Email"/>
+                            <input type="email"/>
                         </fieldset>
-                        <button class="second-btn">Subscribe</button>
+                        <h4>Volunteer Address</h4>
+                        <fieldset>
+                            <label>Address </label>
+                            <input type="text"/>
+                        </fieldset>
+                        <fieldset>
+                            <label>Address Line 2</label>
+                            <input type="text"/>
+                        </fieldset>
+                        <fieldset>
+                            <label>City</label>
+                            <input type="text"/>
+                        </fieldset>
+                        <fieldset>
+                            <label>State/Province/Region</label>
+                            <input type="text"/>
+                        </fieldset>
+                        <fieldset>
+                            <label>Postal / Zip Code</label>
+                            <input type="text"/>
+                        </fieldset>
+                        <fieldset>
+                            <label>Reason for volunteering</label>
+                            <input type="text"/>
+                        </fieldset>
+                        <h4>Volunteer Skills</h4>
+                        <fieldset>
+                            <label>Special Skills</label>
+                            <input type="text"/>
+                        </fieldset>
+                        <button class="second-btn">Submit</button>
                     </form>
             </section>
         </div>
--- webApp/components/List/list.php
+++ webApp/components/List/list.php
@@ -1,10 +1,28 @@
+
+
+<?php
+
+  if(!function_exists("activeStyle")){
+    function activeStyle($link){
+        $uri = $_SERVER['REQUEST_URI'];
+        if($link !== "/"){
+            if(strpos($uri,"$link") !== false){
+                echo "av-active";
+            }
+        }else if($uri === "/"){
+             echo "av-active" ;
+        }
+    }
+  }
+   
+?>
 <?php
 if(!function_exists("navLinks")){
     function navLinks(){
         return array(
                         "Home" => array(
                             "link" => "/",
-                            "class" => "av-active",
+                            "class" => "",
                             "custom" => ""
                                 ),
                         "About MHI" => array(
@@ -37,7 +55,7 @@ if(!function_exists("navLinks")){
         <?php foreach($props as $key => $value) :?>
             <a href="<?php echo $value["link"] ?>">
             <li 
-                class="<?php echo $value["class"] ?>"
+                class="<?php echo $value["class"]?> <?php activeStyle($value["link"]) ?>"
                 <?php echo $value["custom"] ?>
                 ><?php echo $key ?>
             </li>
